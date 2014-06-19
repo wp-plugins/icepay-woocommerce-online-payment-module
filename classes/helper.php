7d460d7e64d74e1312b9288a51fbd50571c4466d
icepay <icepay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-19 08:15:16 +0000

    2.2.9 update
    git-svn-id: https://plugins.svn.wordpress.org/icepay-woocommerce-online-payment-module/tags/2.2.9@934926 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/helper.php
+++ classes/helper.php
@@ -2,7 +2,7 @@
 
 class ICEPAY_Helper {
     
-    private static $version = '2.2.8';
+    private static $version = '2.2.9';
     
     public static function generateListItems($paymentMethods, &$variables) {
         if ($paymentMethods) {
