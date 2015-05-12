98dabe606cfaccf89987d602cbc77f127cdb9c27
icepay <icepay@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 12:05:02 +0000

    older versions
    git-svn-id: https://plugins.svn.wordpress.org/icepay-woocommerce-online-payment-module/tags/2.3.1@1158772 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/placeholder/paymentmethod10.php
@@ -0,0 +1,5 @@
+<?php
+
+class ICEPAY_PaymentMethod_10 extends ICEPAY_Paymentmethod_Core {
+    protected $pmCode = 10;
+}
---
+++ classes/placeholder/paymentmethod5.php
@@ -0,0 +1,5 @@
+<?php
+
+class ICEPAY_PaymentMethod_5 extends ICEPAY_Paymentmethod_Core {
+    protected $pmCode = 5;
+}
